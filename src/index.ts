713a8fa503e1e2eba07efdf9ae1e7c763a175280
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-03-18 21:22:50 -0300

    add to execute local mp3 file

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ require('dotenv').config();
 
 import Discord from'discord.js';
 import axios from'axios';
+import ytdl from'ytdl-core';
+import PlayAudioCommand from './play-audio';
+import fs from 'fs';
+import path from 'path';
+
 
 const client = new Discord.Client();
+const queue = new Map();
 
 client.login(process.env.DISCORD_TOKEN);
 
@@ -11,11 +17,31 @@ client.on('ready', () => {
   console.log(`Logged in as ${client?.user?.tag}!`);
 });
 
-client.on('message', async msg => {
-  if (msg.content.toLowerCase() === '!joke'.toLowerCase()) {
+client.on('message', async message => {
+  if (message.content.toLowerCase() === '!joke'.toLowerCase()) {
     const joke = await getNewJoke();
 
-    msg.reply(joke);
+    message.reply(joke);
+  }
+
+  if (message.content.toLowerCase() === '!calabacon'.toLowerCase()) {
+    message.member?.voice.channel?.join()
+    .then(connection => {
+      console.log('joined channel');
+
+      const filePath = __dirname +'\\assets\\cala-bacon-fera.mp3';
+      connection.play(fs.createReadStream(filePath))
+      // When no packets left to send, leave the channel.
+
+      setTimeout(() => {
+      // connection.channel.leave();
+      message.member?.voice.channel?.leave();
+
+    }, 2000);
+  })
+  .catch(console.error);
+    return;
+
   }
 });
 
