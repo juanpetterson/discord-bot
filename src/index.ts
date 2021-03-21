d01c643828a58d4a8b8bb5aaf74160ad9de1b582
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-03-21 11:42:21 -0300

    add language support

--- src/index.ts
+++ src/index.ts
@@ -14,48 +14,120 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
-  if (message.content.toLowerCase() === '!joke'.toLowerCase()) {
-    const joke = await getNewJoke();
+  try {
+    if (message.content.toLowerCase() === '!joke'.toLowerCase()) {
+      const joke = await getNewJoke();
 
-    message.reply(joke);
+      message.reply(joke);
+    }
+
+    if (message.content.toLowerCase() === '!calabacon'.toLowerCase()) {
+      message.member?.voice.channel?.join()
+      .then(connection => {
+        console.log('joined channel');
+
+        setTimeout(() => {
+          const filePath = './src/assets/audios/cala-bacon-fera.mp3';
+
+          const hasFile = fs.existsSync(filePath);
+          console.log(hasFile)
+
+          connection.play(fs.createReadStream(filePath));
+        }, 2000);
+
+        // When no packets left to send, leave the channel.
+        setTimeout(() => {
+        connection.channel.leave();
+        }, 4000);
+      })
+    .catch(console.error);
+      return;
+
+    }
+
+    if(message.content.toLowerCase().startsWith('!falar')) {
+      const args = message.content.split(' ')
+      args.shift();
+
+      let language = 'pt-br';
+
+      const lastArg = args[args.length -1];
+      const hasLanguageParam = lastArg.startsWith('<') && lastArg.endsWith('>')
+
+      if (hasLanguageParam ) {
+        language = lastArg.replace('<', '').replace('>', '');
+        args.pop();
+      }
+
+
+      await getTextAsVoice(args.join(''), language);
+      executeVoice(message);
+    }
+
+    if(message.content.toLowerCase().startsWith('!speech')) {
+
+      const args = message.content.split(' ')
+      args.shift();
+
+      await getTextAsVoice(args.join(''), 'en');
+      executeVoice(message);
+    }
+  } catch (error) {
+    console.log(error)
   }
 
-  if (message.content.toLowerCase() === '!calabacon'.toLowerCase()) {
-    message.member?.voice.channel?.join()
-    .then(connection => {
-      console.log('joined channel');
-
-      setTimeout(() => {
-        const filePath = '..\\assets\\cala-bacon-fera.mp3';
-        connection.play(fs.createReadStream(filePath), {volume: 1.0});
-      }, 2000);
-
-      // When no packets left to send, leave the channel.
-      setTimeout(() => {
-      connection.channel.leave();
-      }, 4000);
-    })
-  .catch(console.error);
-    return;
-
-  }
-
-  if(message.content.toLowerCase().startsWith('!falar')) {
-
-    const args = message.content.split(' ')
-    args.shift();
-
-    await getTextAsVoice(args.join(''));
-    await executeVoice(message);
-  }
-
-  if(message.content.toLowerCase().startsWith('!speech')) {
-
-    const args = message.content.split(' ')
-    args.shift();
-
-    await getTextAsVoice(args.join(''), 'en');
-    await executeVoice(message);
+  if (message.content.toLowerCase() === '!langs'.toLowerCase()) {
+    message.reply(`'af' : 'Afrikaans'
+    'sq' : 'Albanian'
+    'ar' : 'Arabic'
+    'hy' : 'Armenian'
+    'ca' : 'Catalan'
+    'zh' : 'Chinese',
+    'zh-cn' : 'Chinese (Mandarin/China)'
+    'zh-tw' : 'Chinese (Mandarin/Taiwan)'
+    'zh-yue' : 'Chinese (Cantonese)'
+    'hr' : 'Croatian'
+    'cs' : 'Czech'
+    'da' : 'Danish'
+    'nl' : 'Dutch'
+    'en' : 'English'
+    'en-au' : 'English (Australia)'
+    'en-uk' : 'English (United Kingdom)'
+    'en-us' : 'English (United States)'
+    'eo' : 'Esperanto'
+    'fi' : 'Finnish'
+    'fr' : 'French'
+    'de' : 'German'
+    'el' : 'Greek'
+    'ht' : 'Haitian Creole'
+    'hi' : 'Hindi'
+    'hu' : 'Hungarian'
+    'is' : 'Icelandic'
+    'id' : 'Indonesian'
+    'it' : 'Italian'
+    'ja' : 'Japanese'
+    'ko' : 'Korean'
+    'la' : 'Latin'
+    'lv' : 'Latvian'
+    'mk' : 'Macedonian'
+    'no' : 'Norwegian'
+    'pl' : 'Polish'
+    'pt' : 'Portuguese'
+    'pt-br' : 'Portuguese (Brazil)'
+    'ro' : 'Romanian'
+    'ru' : 'Russian'
+    'sr' : 'Serbian'
+    'sk' : 'Slovak'
+    'es' : 'Spanish'
+    'es-es' : 'Spanish (Spain)'
+    'es-us' : 'Spanish (United States)'
+    'sw' : 'Swahili'
+    'sv' : 'Swedish'
+    'ta' : 'Tamil'
+    'th' : 'Thai'
+    'tr' : 'Turkish'
+    'vi' : 'Vietnamese'
+    'cy' : 'Welsh'`);
   }
 });
 
@@ -66,7 +138,7 @@ const getNewJoke = async (): Promise<string> =>  {
 }
 
 const executeVoice = (message: Discord.Message) => {
-    const filePath = 'speech.mp3';
+    const filePath = './src/assets/audios/speech.mp3';
     message.member?.voice.channel?.join().then(connection => {
       connection.play(fs.createReadStream(filePath));
 
@@ -82,7 +154,7 @@ const executeVoice = (message: Discord.Message) => {
 const getTextAsVoice = async (text: string, language = 'pt-br') => {
   const speech = new gtts(text, language);
 
-  await speech.save("speech.mp3", (response: any) => {
+  await speech.save("./src/assets/audios/speech.mp3", (response: any) => {
     console.log(response)
 
     return true;
