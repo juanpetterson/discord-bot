4c2ef4b30494a7de62afe5ffd9df0c11dbbe83cb
Juan Petterson <juan.petterson@gmail.com>
2023-12-19 12:20:01 -0400

    add jack message in the morning

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
 import express from 'express'
 
+import { client } from './index'
+
 const server = express()
 
 server.all('/', (req, res) => {
-  console.log('Bot is running')
-  res.send('Bot is running: ' + new Date().toISOString())
+  console.log('Bot is running: ' + new Date().toISOString())
+
+  const currentTimeHours = new Date().getHours()
+  const currentTimeMinutes = new Date().getMinutes()
+
+  if (
+    currentTimeHours === 11 &&
+    currentTimeMinutes >= 0 &&
+    currentTimeMinutes <= 5
+  ) {
+    const channel = client.channels.cache.get('1003668690052587623') as any
+
+    if (!channel) return
+
+    channel.send('🦧  <- jaque')
+  }
+
+  res.send('Bot is running')
 })
 
 export const keepAlive = () => {
